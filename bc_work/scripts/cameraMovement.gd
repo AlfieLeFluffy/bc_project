7b8c3e08dc8c9ade5d7a425e2f8601e6efc6f1d4
Tomáš Vlach <tomas-vlach@post.cz>
2024-09-22 02:14:50 +0200

    Initial commit (base Godot project + basic movement/textures)
    Includes:
    - base Godot project files
    - basic player texture
    - basic tile set (rundown apartment)
    - basic movement
    - base camera controller

---
+++ bc_work/scripts/cameraMovement.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var player
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player = get_parent()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		global_position = player.position.lerp(get_global_mouse_position(), 0.3); 
