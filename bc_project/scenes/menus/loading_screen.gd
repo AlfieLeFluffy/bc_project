fc8c604602bf3928005e9e33b04deb760fdf43c4
Tomáš Vlach <tomas-vlach@post.cz>
2024-10-08 01:11:49 +0200

    Added a simple loading screen (no GUI)

---
+++ bc_project/scenes/menus/loading_screen.gd
@@ -0,0 +1,22 @@
+extends MarginContainer
+
+var Scene
+var progress
+var packedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Scene = Global.NextScene
+	print(Scene)
+	ResourceLoader.load_threaded_request(Scene)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	progress = []
+	ResourceLoader.load_threaded_get_status(Scene,progress)
+	print(progress[0]*100)
+	
+	if progress[0] == 1:
+		packedScene = ResourceLoader.load_threaded_get(Scene)
+		get_tree().change_scene_to_packed(packedScene)
--- bc_project/scenes/menus/main_menu.gd
+++ bc_project/scenes/menus/main_menu.gd
@@ -13,7 +13,8 @@ func _process(delta: float) -> void:
 
 
 func _on_test_scene_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/test_level.tscn")
+	Global.NextScene = "res://scenes/levels/test_level.tscn"
+	get_tree().change_scene_to_packed(load("res://scenes/menus/loading_screen.tscn"))
 
 
 func _on_quit_button_pressed() -> void:
