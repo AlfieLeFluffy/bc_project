2edfd2c9754c4976b7fa1678aeb5c3b860dc7493
Tomáš Vlach <tomas-vlach@post.cz>
2025-01-19 19:28:15 +0100

    Added door animations, animation player and tree
    Created animation sprite for a door states (open, closed, opening, closing)
    Added animation player and tree with a state machine to handle the animation transitions
    Fixed a minor bug regarding NPC interactive area colisions

--- bc_project/scenes/interactive/door_sideways.gd
+++ bc_project/scenes/interactive/door_sideways.gd
@@ -3,6 +3,13 @@ extends "res://scenes/interactable.gd"
 @export var opened:bool = false
 @export var locked:bool = false
 
+@onready var state_machine = $AnimationTree["parameters/playback"]
+
+
+# Local ready function for instantiated objects
+func local_ready() -> void:
+	state_machine.start("Start")
+
 # Active function if no dialog detected
 func interact_function() -> void:
 	if locked:
@@ -12,10 +19,23 @@ func interact_function() -> void:
 		opened = not opened
 		$StaticBody2D/CollisionShape2D2.set_deferred("disabled",false)
 		$LightOccluder2D.visible = true
+		state_machine.travel("closing")
 	elif not opened:
 		opened = not opened
 		$StaticBody2D/CollisionShape2D2.set_deferred("disabled",true)
 		$LightOccluder2D.visible = false
+		state_machine.travel("opening")
 
 func toggle_lock() -> void:
 	locked = not locked
+
+
+"""
+--- Activate/deactivate interactivity Rewrite
+"""
+
+func activate_hover() -> void:
+	$Sprite2D.material.set("shader_parameter/line_thickness",1)
+
+func deactivate_hover() -> void:
+	$Sprite2D.material.set("shader_parameter/line_thickness",0)
