43029edda8a3a4f9bf0d4b242aeac2f504950fa2
Tomáš Vlach <tomas-vlach@post.cz>
2025-03-16 19:40:57 +0100

    Reworked elevator script duplicity and persistence methods
    Duplicate code in move_to_stop methods has been moved to a separated into a method.
    Elevator saving has been changed from static variables to saving the entire elevator resource.

--- bc_project/scenes/elevators/elevator.gd
+++ bc_project/scenes/elevators/elevator.gd
@@ -29,27 +29,36 @@ func _physics_process(delta: float) -> void:
 #endregion
 
 #region Incoming Signal Methods
-func move_to_stop_key(id:String, key: String) -> void:
-	if key == resource.currentStop or id != resource.id:
-		return
-	if not resource.stops.has(key):
-		printerr("Error: Elevator stop of key '%s' does not exist in elevator id '%s'" % [key, resource.id])
-		return
-	if resource.active:
-		printerr("Error: Elevator is active, cannot move to stop key '%s' for elevator id '%s'" % [key, resource.id])
+func move_to_stop_key(id:String, key: String, force: bool = false) -> void:
+	if not check_input_valid(id, key, force):
 		return
 	move_to_stop(key)
 
-func move_to_stop_vector(id:String, vector: Vector2) -> void:
-	if id != resource.id:
-		return
-	if not is_instance_valid(vector):
-		printerr("Error: Elevator move to vector needs a valid vector, elevator id '%s'" % resource.id)
-		return
-	if resource.active:
-		printerr("Error: Elevator is active, cannot move to stop vector '%s' for elevator id '%s'" % [str(vector), resource.id])
+func move_to_stop_vector(id:String, vector: Vector2, force: bool = false) -> void:
+	if not check_input_valid(id, vector, force):
 		return
 	move_to_vector(vector)
+	
+func check_input_valid(id: String, stop, force: bool = false) -> bool:
+	if id != resource.id:
+		return false
+	
+	if stop is String:
+		if not resource.stops.has(stop):
+			printerr("Error: Elevator stop of key '%s' does not exist in elevator id '%s'" % [stop, resource.id])
+			return false
+	elif stop is Vector2:
+		if not is_instance_valid(stop):
+			printerr("Error: Elevator move to vector needs a valid vector, elevator id '%s'" % resource.id)
+			return false
+	else:
+		printerr("Error: Elevator move to vector needs a valid vector, elevator id '%s'" % resource.id)
+		return false
+	
+	if resource.active and not force:
+		printerr("Error: Elevator is active, cannot move to stop vector '%s' for elevator id '%s'" % [str(stop), resource.id])
+		return false
+	return true
 #endregion
 
 #region Elevator Managment Methods
@@ -112,9 +121,9 @@ func saving() -> Dictionary:
 		"persistent": true,
 		"nodepath": get_path(),
 		"parent": get_parent().get_path(),
-		"active": resource.active,
-		"currentStop": resource.currentStop,
-		"movingToStop": resource.movingToStop,
+		"resources": {
+			"resource": resource,
+		},
 		"cabin": {
 			"posX" : %ElevatorCabin.position.x,
 			"posY" : %ElevatorCabin.position.y,
@@ -122,15 +131,14 @@ func saving() -> Dictionary:
 	}
 
 func loading(input:Dictionary) -> bool:
-	if input.has_all(["active","currentStop","movingToStop","cabin"]):
-		resource.active = input["active"]
-		resource.currentStop = input["currentStop"]
-		resource.movingToStop = input["movingToStop"]
+	if input.has_all(["resources","cabin"]):
+		if input["resources"].has("resource"):
+			resource = input["resources"]["resource"]
 		if input["cabin"].has_all(["posX","posY"]):
 			%ElevatorCabin.position = Vector2(input["cabin"]["posX"],input["cabin"]["posY"])
 		setup_cabin()
 		if resource.active:
-			move_to_stop(resource.movingToStop)
+			Signals.elevator_move_to_key.emit(resource.id,resource.movingToStop,true)
 		return true
 	return false
 #endregion
