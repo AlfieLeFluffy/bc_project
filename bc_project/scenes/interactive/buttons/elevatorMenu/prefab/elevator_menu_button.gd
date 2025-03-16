acc20c44e656aa7c0936e0de76441dd6719e1f22
Tomáš Vlach <tomas-vlach@post.cz>
2025-03-16 19:48:51 +0100

    Added texturing to elevator menu buttons

--- bc_project/scenes/interactive/buttons/elevatorMenu/prefab/elevator_menu_button.gd
+++ bc_project/scenes/interactive/buttons/elevatorMenu/prefab/elevator_menu_button.gd
@@ -6,7 +6,11 @@ var key: String = ""
 func setup_button(_id: String,_key: String):
 	id = _id
 	key = _key
-	text = key
+	text = tr(key.strip_edges())
+	setup_texture()
+
+func setup_texture() -> void:
+	pass
 
 func _on_pressed() -> void:
 	Signals.emit_signal("elevator_move_to_key",id,key)
