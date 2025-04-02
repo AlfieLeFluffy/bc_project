181e51439b8c5a339773feba69565dc88a32ce87
Tomáš Vlach <tomas-vlach@post.cz>
2025-04-02 23:54:25 +0200

    Reworked main overlay
    Reworked scene structure.
    Reworked existing textures into nine patch textures.
    Removed old textures.
    Reworked and refactored main overlay script.

--- bc_project/scenes/UI/overlay/main_overlay.gd
+++ bc_project/scenes/UI/overlay/main_overlay.gd
@@ -6,11 +6,6 @@ extends Control
 
 var shiftTimeout = true
 
-@onready var MainInfo = $MainInfo
-@onready var TimelineInfo = $MainInfo/TimelineInfo
-@onready var CaseInfo = $MainInfo/CaseInfo
-@onready var TimelineShiftAnimation = $MainInfo/TimelineInfo/TimelineShiftAnimation
-
 """
 --- Setup Methods
 """
@@ -30,21 +25,21 @@ func _process(delta: float) -> void:
 """
 --- UI methods Methods
 """
-func update_UI (Timeline: String = "") -> void:
-	if Timeline != "":
-		TimelineInfo.get_node("Label").text = tr("TIMELINE_UI") + Timeline
+func update_UI (timeline: String = "") -> void:
+	var timelineString: String = "" 
+	if timeline != "":
+		timelineString = timeline
 	else:
-		TimelineInfo.get_node("Label").text = tr("TIMELINE_UI") + Global.Timeline
-	TimelineShiftAnimation.get_node("Label").text = GameController.get_input_key("timeline_shift")
+		timelineString = Global.Timeline
 	
-	#CaseInfo.get_node("Lable").text = "Case: " + Global.Case
+	%TimelineLabel.text = "[color=#%s]%s[/color][color=#%s]%s[/color]" % [Global.color_White.to_html(),tr("TIMELINE_UI"),Global.color_White.to_html(), timelineString]
+	
+	%TimelineKeyLabel.text = "[color=#%s]%s[/color]" % [Global.color_White.to_html(),GameController.get_input_key("timeline_shift")]
 
 """
 --- Timeline shit Methods
 """
 func shift_sequence() -> void:
-	TimelineShiftAnimation.play("timeout")
-	GameController.emit_signal("playScreenEffect",GameController.screenEffectEnum.TIMELINE_SHIFT)
 	shift_timeout_counter()
 
 func shift_timeout_counter() -> void:
