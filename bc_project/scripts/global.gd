4d04c61e2826c7f482f3ac894ebe640bdc64fee9
Tomáš Vlach <tomas-vlach@post.cz>
2025-04-02 23:52:02 +0200

    Added colour scheme constants
    Added colour scheme constants for reference in other files and easier changes.
    Partially refactored global script.

--- bc_project/scripts/global.gd
+++ bc_project/scripts/global.gd
@@ -1,22 +1,29 @@
 extends Node
 
 """
---- GLobal variables
+--- GLobal Variables and Constants
 """
+#region Timeshift Variables
 # Veriables used in timeline hoping and UI elements
 var Timeline = "___"
 
 var TimelineShiftReady = true
 const TIMELINE_TIMEOUT = 1.0
 const TIMELINE_SHIFT_OFFSET = 0.2
+#endregion
 
+#region Character Variables
 # Main character info
 @onready var playerCharacterNode: Node = get_tree().get_first_node_in_group("Player")
 const mainCharacterName = "Alfie"
+#endregion
 
+#region Group Name Constants
 # Main character info
 const cameraFocusName = "CameraFocus"
+#endregion
 
+#region Board Variables and Constants
 # Veriables used in board control
 var Active_Interactive_Item
 
@@ -25,17 +32,28 @@ var board_elements: Dictionary
 var line_elements: Dictionary
 
 const BOARD_LINE_OFFSET: Vector2 = Vector2(-16,-16)
+#endregion
 
-"""
---- Scene variables
-"""
-
+#region Scene Constants
 var currentScene: String
 const scenePaths: Dictionary = {
 	"main_menu": "res://scenes/menus/main_menu.tscn",
 	"test_level": "res://scenes/levels/test_level.tscn"
 }
 const nongameplayScenes: Array[String] = ["MainMenu"]
+#endregion
+
+"""
+--- Color Constants
+"""
+#region Color Constants
+const color_White: Color = Color("e1f6fa")
+const color_TextBright: Color = Color("a2ebfa")
+const color_TextHighlight: Color = Color("5fcfe6")
+const color_Highlight: Color = Color("b334a4")
+const color_DarkBackground: Color = Color("2c2c39")
+#endregion
+
 
 """
 --- Setup Methods
