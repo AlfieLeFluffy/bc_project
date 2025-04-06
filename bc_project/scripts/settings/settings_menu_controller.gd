845939b14a5fb5dc89a71588d37bd27d2eb66369
Tomáš Vlach <tomas-vlach@post.cz>
2025-04-06 03:59:26 +0200

    Fixed setting menu and removed unused settings
    Fixed settings menu not changing configuration due to lost signals during last changes.
    Removed keep task visible setting as it is no longer needed.

--- bc_project/scripts/settings/settings_menu_controller.gd
+++ bc_project/scripts/settings/settings_menu_controller.gd
@@ -50,7 +50,6 @@ func close_menu() -> void:
 """
 func setup_gameplay() -> void:
 	%LanguageOptionButton.selected = LANG_DICT.find_key(config.get_value("Gameplay", "Language"))
-	%TaskKeepVisibleButton.selected = config.get_value("Gameplay", "KeepTaskVisible")
 
 func setup_graphics() -> void:
 	%ModeOptionButton.selected = config.get_value("Graphics", "ScreenMode")
@@ -79,8 +78,7 @@ func _on_language_option_button_item_selected(index: int) -> void:
 	if LANG_DICT.has(index):
 		config.set_value("Gameplay", "KeepTaskVisible", LANG_DICT[index])
 
-func _on_task_keep_visible_button_item_selected(index: int) -> void:
-	config.set_value("Gameplay", "KeepTaskVisible", index)
+
 
 func _on_mode_option_button_item_selected(index: int) -> void:
 	config.set_value("Graphics", "ScreenMode", index)
@@ -92,19 +90,14 @@ func _on_vsync_option_button_item_selected(index: int) -> void:
 	config.set_value("Graphics", "Vsync", index)
 
 
+func _on_master_slider_value_changed(value: float) -> void:
+	config.set_value("AudioVolume", "Master", value)
 
-func _on_master_slider_drag_ended(value_changed: bool) -> void:
-	if value_changed:
-		config.set_value("AudioVolume", "Master", %MasterSlider.value)
+func _on_sound_slider_value_changed(value: float) -> void:
+		config.set_value("AudioVolume", "SFX", value)
 
-func _on_sound_slider_drag_ended(value_changed: bool) -> void:
-	if value_changed:
-		config.set_value("AudioVolume", "SFX", %SoundSlider.value)
+func _on_music_slider_value_changed(value: float) -> void:
+		config.set_value("AudioVolume", "Music", value)
 
-func _on_music_slider_drag_ended(value_changed: bool) -> void:
-	if value_changed:
-		config.set_value("AudioVolume", "Music", %MusicSlider.value)
-
-func _on_dialogue_slider_drag_ended(value_changed: bool) -> void:
-	if value_changed:
-		config.set_value("AudioVolume", "Dialogue", %DialogueSlider.value)
+func _on_dialogue_slider_value_changed(value: float) -> void:
+		config.set_value("AudioVolume", "Dialogue", value)
