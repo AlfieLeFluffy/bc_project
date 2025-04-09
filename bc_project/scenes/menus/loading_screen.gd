e976d6be376e96054864d0fa8f12941cd3a70600
Tomáš Vlach <tomas-vlach@post.cz>
2025-04-09 02:31:33 +0200

    Reworked loading screen UI
    Added new progress loading textures.
    Reworked loading screen script.
    Added a game logo to the loading screen.

--- bc_project/scenes/menus/loading_screen.gd
+++ bc_project/scenes/menus/loading_screen.gd
@@ -1,20 +1,36 @@
-extends MarginContainer
+class_name LoadingScreen extends CanvasLayer
 
-var Scene
-var progress
-var packedScene
+#region Runtime Variables
+var sceneToLoad: String
+var progressArray: Array = []
+var packedScene: Resource
+var lastUpdate: float = 0.0
 
-# Called when the node enters the scene tree for the first time.
+var tween: Tween
+#endregion
+
+#region Setup Methods
 func _ready() -> void:
-	Scene = GameController.sceneToLoad
-	ResourceLoader.load_threaded_request(Scene)
+	sceneToLoad = GameController.sceneToLoad
+	ResourceLoader.load_threaded_request(sceneToLoad)
+#endregion
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+#region Runtime Methods
 func _process(delta: float) -> void:
-	progress = []
-	ResourceLoader.load_threaded_get_status(Scene,progress)
-	$TextureProgressBar.value = progress[0]*100
-	if progress[0] == 1:
-		packedScene = ResourceLoader.load_threaded_get(Scene)
+	ResourceLoader.load_threaded_get_status(sceneToLoad,progressArray)
+	if lastUpdate < progressArray[0]:
+		lastUpdate = progressArray[0]
+		update_progress()
+
+func update_progress() -> void:
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	tween.tween_property(%ProgressBar,"value",lastUpdate,0.1).set_ease(Tween.EASE_IN_OUT)
+	if progressArray[0] >= 1.0:
+		packedScene = ResourceLoader.load_threaded_get(sceneToLoad)
+		await tween.finished
 		get_tree().change_scene_to_packed(packedScene)
+#endregion
