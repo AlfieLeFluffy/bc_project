210eb869323321a640ad8757170e1314f9e16dad
Tomáš Vlach <tomas-vlach@post.cz>
2025-04-09 21:04:23 +0200

    Reworked saving/loading animation and minor fixes
    Reworked the saving/loading animation.
    Added loading animation to the loading screen.
    Fixed detective board label alignment issues.

--- bc_project/scripts/persistence/saving_animation.gd
+++ bc_project/scripts/persistence/saving_animation.gd
@@ -4,15 +4,13 @@ class_name SavingAnimation extends CanvasLayer
 var done: bool = false
 
 func _ready() -> void:
+	%AnimationRect.texture.current_frame = 0
 	timeout()
 
 ## Controls animation until saving is done
 func _process(delta: float) -> void:
-	if %AnimationRect.texture.current_frame >= 8 and not done:
-		%AnimationRect.texture.current_frame = 8
-	elif %AnimationRect.texture.current_frame >= 8 and done:
-		if %AnimationRect.texture.current_frame >= 11:
-			queue_free()
+	if %AnimationRect.texture.current_frame >= 15 and done:
+		queue_free()
 
 func timeout() -> void:
 	await get_tree().create_timer(60.0).timeout
